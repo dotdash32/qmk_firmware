3a8d5063de786679135c1ce51b27aaa92df21057
Josh DeWitt <dotdash32@gmail.com>
2026-06-10 01:24:21 -0700

    chore: brm keymap, hrm config.h
    clean up of loose ends

---
+++ keyboards/cameldox38/keymaps/dotdash-brm/keymap.c
@@ -0,0 +1,68 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _QWERTY,
+    _NAV_SIM,
+    _NAV,
+    _SYM,
+    _NUM,
+    _BLANK
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┐    ┌───┬───┬───┬───┬───┐
+     * │ Q │ W │ E │ R │ T │    │ Y │ U │ I │ P │ P │
+     * ├───┼───┼───┼───┼───┤    ├───┼───┼───┼───┼───┤
+     * │ A │ S │ D │ F │ G │    │ H │ J │ K │ L │ B │
+     * ├───┼───┼───┼───┼───┤    ├───┼───┼───┼───┼───┤
+     * |   │ Z │ X │ C │ V │    │ N │ M │ , │ . │ / │
+     * └───┴───┴───┴───┴───┘    └───┴───┴───┴───┴───┘
+
+     *     ┌───┬───┬───┬───┐    ┌───┬───┬───┬───┐
+     *     │LT1|LT2|LT3|LT4|    |RT4|RT3|RT2|RT1|
+     *     └───┴───┴───┴───┘    └───┴───┴───┴───┘
+     */
+    [_QWERTY] = LAYOUT_ortho_3x10_8(
+    //  |             |             |               |             |             |             |             |              |             |
+        KC_Q,         KC_W,         KC_E,           KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,          KC_O,         KC_P,
+        LSFT_T(KC_A), LGUI_T(KC_S), LT(_SYM, KC_D), LCTL_T(KC_F), LALT_T(KC_G), RALT_T(KC_H), RCTL_T(KC_J), LT(_SYM,KC_K), RALT_T(KC_L), RSFT_T(KC_B)
+        KC_NO,        KC_Z,         KC_X,           LGUI_T(KC_C), KC_V,         RGUI_T(KC_N), KC_M,         KC_COMM,       KC_DOT,          _______
+                      // Left then right Thumbs
+                      KC_LALT,        KC_LSFT, LT(_NUM, KC_BSPC), LGUI_T(KC_ESC),
+                      LALT_T(KC_ESC), KC_ENT,  LT(_NAV, KC_SPC),  MO(_NAV)
+                    ),
+
+    [_NAV_SIM] = LAYOUT_ortho_3x10_8(
+    //  |             |             |               |             |              |                |                |                 |                 |
+        _______,      _______,      _______,        _______,      _______,       KC_PGUP,         KC_HOME,         KC_UP,            KC_END,           KC_DEL,
+        _______,      _______,      _______,        KC_TRNS,      _______,       RALT_T(KC_PGDN), RCTL_T(KC_LEFT), LT(_SYM,KC_DOWN), RALT_T(KC_RIGHT), RSFT_T(KC_ENT),
+        _______,      _______,      _______,        _______,      _______,       KC_MPRV,         KC_MNXT,         KC_MPLY,          KC_VOLU,          KC_VOLD,
+                      _______,      _______,        _______,      _______,       _______,         _______,         _______,          _______
+    ),
+    [_NAV] = LAYOUT_ortho_3x10_8(
+    //  |             |             |               |             |               |               |                |                 |                 |
+        KC_Q,         KC_BSPC,      KC_DEL,         KC_ESC,       KC_T,           KC_HOME,         KC_PGDN,         KC_PGUP,          KC_END,            KC_NO,
+        LSFT_T(KC_A), LGUI_T(KC_S), LT(_SYM, KC_D), LCTL_T(KC_F), LALT_T(KC_TAB), RALT_T(KC_LEFT), RCTL_T(KC_DOWN), LT(_SYM,KC_UP), RALT_T(KC_RIGHT), RSFT_T(KC_ENT),
+                      KC_Z,         KC_X,           LGUI_T(KC_C), KC_V,           KC_LBR,          RGUI_T(KC_RBR),  _______,        _______,       _______,      _______,
+                      _______,      _______,        _______,      _______,        _______,         _______,      _______,       _______
+    ),
+
+    [_SYM] = LAYOUT_ortho_3x10_8(
+    //  |             |             |             |             |             |             |             |             |             |
+        KC_1,         KC_2,         KC_3,         KC_4,         KC_5,         KC_6,         KC_7,         KC_8,         KC_9,         KC_0,
+        _______,      _______,      _______,      KC_TRNS,      _______,      _______,      KC_TRNS,      _______,      _______,      KC_QUOTE,
+        _______,       KC_GRV,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,
+                      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
+    ),
+    [_BLANK] = LAYOUT_ortho_3x10_8(
+    //  |             |             |             |             |             |             |             |             |             |
+        _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,
+        _______,      _______,      _______,      KC_TRNS,      _______,      _______,      KC_TRNS,      _______,      _______,      _______,
+        _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,
+                      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
+    ),
+};
